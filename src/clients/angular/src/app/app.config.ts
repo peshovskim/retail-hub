0fa6a64faab5f3360972b2a25023eaca3d11448c
mpeshovski <mps@zenegy.dev>
2026-04-02 17:20:37 +0200

    ngrx state management package added

--- src/clients/angular/src/app/app.config.ts
+++ src/clients/angular/src/app/app.config.ts
@@ -1,11 +1,26 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideStore(),
+    provideEffects([]),
+    ...(isDevMode()
+      ? [
+          provideStoreDevtools({
+            maxAge: 25,
+            logOnly: false
+          })
+        ]
+      : [])
   ]
 };
