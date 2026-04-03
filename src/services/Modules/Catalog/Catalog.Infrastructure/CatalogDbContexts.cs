7b0e8454a2a3173280c035acc8ec7fa281add192
mpeshovski <mps@zenegy.dev>
2026-04-03 16:47:08 +0200

    no tracking for the queries as general begaviour

--- src/services/Modules/Catalog/Catalog.Infrastructure/CatalogDbContexts.cs
+++ src/services/Modules/Catalog/Catalog.Infrastructure/CatalogDbContexts.cs
@@ -3,14 +3,25 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Catalog.Infrastructure;
 
-public sealed class CatalogWriteDbContext(DbContextOptions<CatalogWriteDbContext> options) : DbContext(options)
+public sealed class CatalogWriteDbContext : DbContext
 {
+    public CatalogWriteDbContext(DbContextOptions<CatalogWriteDbContext> options)
+        : base(options)
+    {
+    }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder) =>
         modelBuilder.ApplyConfiguration(new CategoryWriteConfiguration());
 }
 
-public sealed class CatalogReadDbContext(DbContextOptions<CatalogReadDbContext> options) : DbContext(options)
+public sealed class CatalogReadDbContext : DbContext
 {
+    public CatalogReadDbContext(DbContextOptions<CatalogReadDbContext> options)
+        : base(options)
+    {
+        ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+    }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder) =>
         modelBuilder.ApplyConfiguration(new CategoryReadConfiguration());
 }
